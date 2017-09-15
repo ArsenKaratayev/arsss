6357c72a4432ed999b91c6980b3a623d9ad07610
Arsen Karatayev <arsenkaratayev@gmail.com>
2017-09-15 18:22:20 +0600

    lab4

--- Program.cs
+++ Program.cs
@@ -8,5 +8,31 @@ namespace git2
         {
             Console.WriteLine("Hello World!");
         }
+        static void isPrime(int n)
+        {
+            int k = 0;
+            if (n < 3)
+            {
+                System.Console.WriteLine("prime");
+            }
+            for (int i = 0; i < Math.Pow(n, 0.5); i++)
+            {
+                if (n % i == 0)
+                {
+                    k++;
+                    if (k > 1)
+                    {
+                        System.Console.WriteLine("composite");
+                    }
+                }
+            }
+            if (k == 1)
+            {
+                System.Console.WriteLine("prime");
+            }else
+            {
+                System.Console.WriteLine("composite");
+            }
+        }
     }
 }
